fb9776bbaf06e70b73dddff6b078615ee72d2e42
Paweł Laszczkowski <pawel.laszczkowski@gmail.com>
2025-10-26 21:41:10 +0100

    Update

--- src/BlazorLens.Web/GlobalUsings.cs
+++ src/BlazorLens.Web/GlobalUsings.cs
@@ -1,5 +1,2 @@
-﻿global using Microsoft.AspNetCore.Components;
-global using Microsoft.AspNetCore.Components.Web;
-
 global using BlazorLens.Application;
 global using BlazorLens.Infrastructure;
--- src/BlazorLens.Web/Program.cs
+++ src/BlazorLens.Web/Program.cs
@@ -1,7 +1,11 @@
+using BlazorLens.Web.Components;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
@@ -11,13 +15,16 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+
 app.UseAntiforgery();
 
-app.MapRazorComponents<BlazorLens.Web.Components.Routes>().AddInteractiveServerRenderMode();
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
